f39286d5f761708b07dcf3b08422382e7f50d4ab
sarahdarwishh <sarahdarwish98@yahoo.com>
2021-09-06 02:58:17 +0300

    app

---
+++ src/components/HornedBeast.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+ class HornedBeast extends Component {
+    render() {
+        return (
+            <div>
+               <h2>{ this.props.title}</h2> 
+               <img src={this.props.image_url} alt='img' title={this.props.title}/>
+               
+               <p>{ this.props.description}</p>
+               <h4>{ this.props.keyword}</h4>
+               <h4>{ this.props.horns}</h4>
+               
+            </div>
+        )
+    }
+}
+
+export default HornedBeast
